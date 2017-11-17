89bc8cd4a9781bf5c9fcfe9e6f010e593463ab2e
Asher Dunn <asher.github@gmail.com>
2017-11-16 22:53:54 -0800

    plot freqz for all filters actually used, instead of making separate ones

--- gaussian.py
+++ gaussian.py
@@ -14,18 +14,23 @@ def make_input(dt):
 def apply_window(t_input, x_input, dt, width, func):
     t_result = np.arange(0.0, 2.0, dt)
     x_result = np.zeros(np.size(t_result))
+    filters = [None] * len(t_result)
 
     for i in range(len(t_result)):
         mask = np.logical_and(t_input >= t_result[i] - width, t_input < t_result[i])
         t_window = t_input[mask] - (t_result[i] - width / 2.0)
         x_window = x_input[mask]
 
-        x_result[i] = func(width, t_window, x_window)
+        x_result[i], filters[i] = func(width, t_window, x_window)
 
-    return t_result, x_result
+    return t_result, x_result, filters
 
 def windowed_avg_func(width, t, x):
-    return np.average(x) if len(x) > 0 else np.nan
+    if len(x) == 0:
+        return np.nan, None
+
+    filt = np.ones(np.size(t)) / len(t)
+    return np.dot(x, filt), filt
 
 def make_gaussian_filter(width, t):
     stddev = width / 6.75
@@ -38,18 +43,18 @@ def make_gaussian_filter(width, t):
 
 def windowed_gaussian_func(width, t, x):
     if len(x) == 0:
-        return np.nan
+        return np.nan, None
 
     filt = make_gaussian_filter(width, t)
-    return np.dot(filt, x)
+    return np.dot(filt, x), filt
 
 if __name__ == '__main__':
     dt = 0.02
     window = 0.1
 
     (t, x) = make_input(dt)
-    (ta, xa) = apply_window(t, x, dt, window, windowed_avg_func)
-    (tg, xg) = apply_window(t, x, dt, window, windowed_gaussian_func)
+    (ta, xa, filters_a) = apply_window(t, x, dt, window, windowed_avg_func)
+    (tg, xg, filters_g) = apply_window(t, x, dt, window, windowed_gaussian_func)
 
     plt.figure()
     plt.plot(t, x, '.-', color='black')
@@ -61,26 +66,30 @@ if __name__ == '__main__':
 
     plt.figure()
 
-    ba = [0.2, 0.2, 0.2, 0.2, 0.2]
-    aa = [1.0]
+    for b in filters_a:
+        if b is None:
+            continue
+        w, h = sig.freqz(b, [1.0])
+        angles = np.unwrap(np.angle(h))
+        plt.subplot(211)
+        plt.plot(w, 20 * np.log10(abs(h)), color='blue')
+        plt.subplot(212)
+        plt.plot(w, angles, color='blue')
 
-    bg = make_gaussian_filter(window, [-0.04, -0.02, 0.0, 0.02, 0.04])
-    ag = [1.0]
-
-    wa, ha = sig.freqz(ba, aa)
-    angles_a = np.unwrap(np.angle(ha))
-
-    wg, hg = sig.freqz(bg, ag)
-    angles_g = np.unwrap(np.angle(hg))
+    for b in filters_g:
+        if b is None:
+            continue
+        w, h = sig.freqz(b, [1.0])
+        angles = np.unwrap(np.angle(h))
+        plt.subplot(211)
+        plt.plot(w, 20 * np.log10(abs(h)), color='red')
+        plt.subplot(212)
+        plt.plot(w, angles, color='red')
 
     plt.subplot(211)
-    plt.plot(wa, 20 * np.log10(abs(ha)), color='blue')
-    plt.plot(wg, 20 * np.log10(abs(hg)), color='red')
+    plt.ylim(-40, 0)
     plt.grid()
-
     plt.subplot(212)
-    plt.plot(wa, angles_a, color='blue')
-    plt.plot(wg, angles_g, color='red')
     plt.grid()
 
     plt.savefig('gaussian-freqz.png')
